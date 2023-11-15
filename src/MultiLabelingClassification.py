09aa864460524c9e41255dc4c8bf619f76e044dc
KarateLucario1999 <ja.mueller@aol.de>
2023-11-15 12:57:35 +0100

    init multilabeling test with resampling rate=4s , learning rate= 0.0001, Dropout layers and an additional fully connected layer

--- src/MultiLabelingClassification.py
+++ src/MultiLabelingClassification.py
@@ -23,7 +23,7 @@ RESAMPLING_RATE = "4s"
 def start_task():
     task = Task.init(project_name='MultiLabeling_Classification_Test',
                      task_name=f'Experiment Test MultiLabeling (resampling rate= 4s, learning_rate=0.0001,'
-                               f' w\\ Dropout)')  # f' additional layer)')
+                               f' w\\ Dropout, additional layer)')
     task.execute_remotely(queue_name='default', clone=False, exit_process=True)
 
     # get local copy of DataBases
