c11070927ac465fc722695ba67fdcfd15b0903ea
KarateLucario1999 <ja.mueller@aol.de>
2023-11-17 19:52:18 +0100

    init multilabeling test with resampling rate=4s , learning rate= 0.00001, Dropout layers and an additional fully connected layer

--- src/MultiLabelingClassification.py
+++ src/MultiLabelingClassification.py
@@ -26,7 +26,7 @@ def start_task():
                                f'resampling rate= 4s,'
                                # f' activation_function=leaky_relu,'
                                f' learning_rate=0.00001,'
-                               # f' w\\ Dropout,'
+                               f' w\\ Dropout,'
                                f' additional layer)')
     task.execute_remotely(queue_name='default', clone=False, exit_process=True)
 
